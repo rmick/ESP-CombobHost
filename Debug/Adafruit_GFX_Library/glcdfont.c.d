ed940ee807cd28692b1628c28d80a16bd847c8e9
Richie Mickan <rmick@users.noreply.github.com>
2021-06-07 09:29:02 +1000

    Delete Debug directory

--- Debug/Adafruit_GFX_Library/glcdfont.c.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Richie\AppData\Local\Temp\VMBuilds\ESP-CombobHost\esp32_heltec_wifi_kit_32\Debug\Adafruit_GFX_Library\glcdfont.c.o: \
- C:\Users\Mickan-Dropbox\Dropbox\Arduino\libraries\Adafruit_GFX_Library\glcdfont.c
--- Debug/VM_DBG/VM_mem_check_sam.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Richie\AppData\Local\Temp\VMBuilds\ESP-CombobHost\esp32_heltec_wifi_kit_32\Debug\VM_DBG\VM_mem_check_sam.cpp.o: \
- c:\program\ files\ (x86)\microsoft\ visual\ studio\2019\community\common7\ide\extensions\c2d2aslw.nu3\Micro\ Platforms\default\debuggers\VM_DBG\VM_mem_check_sam.cpp
