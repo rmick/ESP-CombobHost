4445e019685bae307cd8dc1c70f357f9a2179958
Richie Mickan <rmick@users.noreply.github.com>
2021-06-07 09:29:19 +1000

    Delete Release directory

--- Release/IRremote2016/irPronto.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Richie\AppData\Local\Temp\VMBuilds\ESP-CombobHost\esp32_heltec_wifi_kit_32\Release\IRremote2016\irPronto.cpp.o: \
- C:\Users\Mickan-Dropbox\Dropbox\Arduino\libraries\IRremote2016\irPronto.cpp
